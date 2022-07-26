3e1b4b42555ef0b02a0e7ae29fea458d404bc633
Elman Dodge <elmandodge@gmail.com>
2022-07-26 18:46:48 +0200

    testing2

--- js/routes.js
+++ js/routes.js
@@ -16,7 +16,7 @@ const handleLocation = async () => {
     const path = window.location.pathname;
     const route = routes[path] || routes[404];
     const html = await fetch(route).then((data) => data.text());
-    document.getElementById("main-page").innerHTML = text;
+    document.getElementById("wrapper").innerHTML = text;
 };
 
 window.onpopstate = handleLocation;
